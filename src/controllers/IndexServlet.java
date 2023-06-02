9cca1322546e2dcc39604fb66f9b92a1accf22b3
haruko.kawano <2023dounyukenshu.026@gmail.com>
2023-06-02 23:04:41 +0900

    単語共有機能追加、とりあえず共有、あとで変更予定

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -14,23 +14,14 @@ import javax.servlet.http.HttpServletResponse;
 import models.MyCard;
 import utils.DBUtil;
 
-/**
- * Servlet implementation class IndexServlet
- */
 @WebServlet("/index")
 public class IndexServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public IndexServlet() {
         super();
     }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
